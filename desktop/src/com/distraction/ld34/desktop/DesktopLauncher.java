f948cbe8c496ebda30d45f1c98aaa0070f52595f
Mike S <foreign.guy.mike@gmail.com>
2015-12-11 21:24:22 -0500

    init

---
+++ desktop/src/com/distraction/ld34/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.distraction.ld34.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.distraction.ld34.LD34;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LD34(), config);
+	}
+}
