a495c0ec420f8a95b0f6fd7d9d376be1c8536e08
Mike S <foreign.guy.mike@gmail.com>
2015-12-13 16:34:06 -0500

    change title

--- core/src/com/distraction/ld34/Vars.java
+++ core/src/com/distraction/ld34/Vars.java
@@ -2,7 +2,7 @@ package com.distraction.ld34;
 
 public class Vars {
 	
-	public static final String TITLE = "TBD";
+	public static final String TITLE = "Fertilized Crops Xtreme";
 	public static final int WIDTH = 400;
 	public static final int HEIGHT = 300;
 	public static final int SCALE = 2;
